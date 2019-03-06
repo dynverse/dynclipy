2e089870dba68c5e2a77ebb2dffb552b24c2b0cc
Wouter Saelens <wouter.saelens@gmail.com>
2019-03-06 13:24:41 +0100

    fix travis

--- dynclipy/__init__.py
+++ dynclipy/__init__.py
@@ -1,3 +1,4 @@
 import rpy2.robjects as ro
 
 from .read import check_conversion_rpy2py, main
+from .write import write_output
---
+++ dynclipy/write.py
@@ -0,0 +1,35 @@
+import rpy2.robjects as ro
+import rpy2.rinterface as rinterface
+
+from rpy2.robjects import pandas2ri
+pandas2ri.activate()
+
+from rpy2.robjects import numpy2ri
+numpy2ri.activate()
+
+def write_output(x, file, output_ids):
+    assert isinstance(x, dict)
+
+    ro.globalenv["x"] = x
+    ro.globalenv["file"] = file
+    ro.globalenv["output_ids"] = output_ids
+    ro.r("dyncli::write_output(x = x, file = file, output_ids = output_ids)")
+
+
+@ro.conversion.py2rpy.register(dict)
+def convert_dict(obj):
+    return ro.ListVector(obj)
+
+
+@ro.conversion.py2rpy.register(list)
+def convert_dict(obj):
+    if all([isinstance(x, str) for x in obj]):
+        return ro.StrVector(obj)
+    elif all([isinstance(x, int) for x in obj]):
+        return ro.IntVector(obj)
+    elif all([isinstance(x, bool) for x in obj]):
+        return ro.BoolVector(obj)
+    elif all([isinstance(x, float) for x in obj]):
+        return ro.FloatVector(obj)
+
+    return ro.ListVector(obj)
